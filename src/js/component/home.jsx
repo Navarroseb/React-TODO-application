88ee4fcf286dc1cc50fa0e75d9aa0abc9e61b196
Navarroseb <sebastnavarro@gmail.com>
2022-04-24 22:29:40 +0000

    ending project

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
+	const [newItem, setNewItem] = useState("");
+	const [todo, setTodo] = useState([]);
+	const [counter, setCounter] = useState(0);
+	const addTodo = (e) => {
+		if (newItem === "") {
+			return;
+		}
+		if (e.key === "Enter") {
+			const item = {
+				id: Math.floor(Math.random() * 1000),
+				value: newItem,
+			};
+			setTodo((list) => [...list, item]);
+			setCounter((count) => count + 1);
+			setNewItem("");
+		}
+	};
+
+	const deleteItem = (id) => {
+		const newArray = todo.filter((item) => item.id !== id);
+		setTodo(newArray);
+	};
+
+	const decreaseItem = () => {
+		setCounter((count) => count - 1);
+	};
 	return (
-		<div>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="main-container">
+			<h1 className="text-center mt-5">TO DO LIST</h1>
+			<div className="card-of-list">
+				<input
+					type="text"
+					className="add-todo"
+					placeholder="Add a new to do..."
+					onChange={(e) => {
+						setNewItem(e.target.value);
+					}}
+					value={newItem}
+					onKeyPress={addTodo}
+				/>
+				<ul className="group-list">
+					{todo.map((item) => {
+						return (
+							<li className="items" key={item.id}>
+								{item.value}
+								<button
+									className="delete-item"
+									onClick={() => {
+										deleteItem(item.id);
+										decreaseItem();
+									}}>
+									<i class="fas fa-trash-alt"></i>
+								</button>
+							</li>
+						);
+					})}
+				</ul>
+				<h5>{`${counter} items left`}</h5>
+			</div>
 		</div>
 	);
 };
